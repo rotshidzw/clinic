a051b75237615d1dd3e035d9203359dfbe745ce9
rotshidzw <69056906+rotshidzw@users.noreply.github.com>
2022-09-15 14:38:45 +0200

    adding table branch

--- queries.sql
+++ queries.sql
@@ -1,3 +1,18 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+
+SELECT * FROM animals WHERE name LIKE '%mon';
+	
+SELECT name FROM animals WHERE EXTRACT(YEAR FROM date_of_birth) BETWEEN 2016 AND 2019;
+
+SELECT name FROM animals WHERE neutered = TRUE AND escape_attempts < 3;
+	
+select date_of_birth from animals where name in ('Agumon', 'Pikachu');
+ 
+SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
+	
+SELECT * FROM animals WHERE neutered = TRUE;
+	
+SELECT * FROM animals WHERE NOT name = 'Gabumon';
+
+SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
--- schema.sql
+++ schema.sql
@@ -1,5 +1,11 @@
 /* Database schema to keep the structure of entire database. */
 
+
 CREATE TABLE animals (
-    name varchar(100)
+    id INTEGER primary key GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(250),
+    date_of_birth DATE,
+    escape_attempts INTEGER,
+    neutered BOOLEAN,
+    weight_kg DECIMAL
 );
