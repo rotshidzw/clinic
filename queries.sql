968a1a5d28442c31471eb40a6399ad17a8fa5e30
rotshidzw <69056906+rotshidzw@users.noreply.github.com>
2022-09-17 00:28:48 +0200

    add

--- queries.sql
+++ queries.sql
@@ -37,3 +37,19 @@ SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
 
 -- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
 SELECT species, AVG(escape_attempt) FROM animals WHERE EXTRACT(YEAR FROM date_of_birth) BETWEEN 1990 AND 2000 GROUP BY species;
+
+--third
+
+SELECT * FROM animals INNER JOIN owners ON animals.owner_id = owners.id WHERE owners.full_name = 'Melody Pond';
+
+SELECT * FROM animals INNER JOIN species ON animals.species_id = species.id WHERE species.name = 'pokemon';
+
+SELECT * FROM animals FULL OUTER JOIN owners ON animals.owner_id = owners.id;
+
+SELECT COUNT(*), species.name FROM animals INNER JOIN species ON animals.species_id = species.id GROUP BY species.name;
+
+SELECT * FROM animals INNER JOIN owners ON animals.owner_id = owners.id INNER JOIN species ON animals.species_id = species.id WHERE owners.full_name = 'Jennifer Orwell' AND species.name = 'digimon';
+
+SELECT * FROM animals INNER JOIN owners ON animals.owner_id = owners.id WHERE owners.full_name = 'Dean Winchester' AND escape_attempt = 0;
+
+SELECT full_name, COUNT(full_name) AS animals_owned FROM owners INNER JOIN animals ON animals.owner_id = owners.id GROUP BY full_name ORDER BY COUNT(full_name) DESC;
--- schema.sql
+++ schema.sql
@@ -10,3 +10,10 @@ CREATE TABLE animals (
 );
 
 ALTER TABLE animals ADD COLUMN species VARCHAR(250);
+
+--third
+CREATE TABLE owners ( id INTEGER primary key GENERATED ALWAYS AS IDENTITY, full_name VARCHAR(250), age INTEGER);
+
+CREATE TABLE species ( id INTEGER primary key GENERATED ALWAYS AS IDENTITY, name VARCHAR(250));
+
+ALTER TABLE animals DROP COLUMN species, ADD COLUMN species_id INTEGER, ADD FOREIGN KEY (species_id) REFERENCES species(id), ADD COLUMN owner_id INTEGER, ADD FOREIGN KEY (owner_id) REFERENCES owners(id);
