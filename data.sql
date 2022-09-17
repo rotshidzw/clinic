3a43a61fecee499a8bf47c95f47080d88216009e
rotshidzw <69056906+rotshidzw@users.noreply.github.com>
2022-09-17 13:56:12 +0200

    add

--- data.sql
+++ data.sql
@@ -61,3 +61,38 @@ VALUES
             4
     );
 
+
+ --third
+INSERT INTO owners
+    (full_name, age)
+VALUES
+    ('Sam Smith', 34),
+    ('Jennifer Orwell', 19),
+    ('Bob', 45),
+    ('Melody Pond', 77),
+    ('Dean Winchester', 14),
+    ('Jodie Whittaker', 38);
+
+SELECT * from  owners;
+
+INSERT INTO species (name) VALUES ('pokemon'),  ('digimon');
+
+select * from species;
+
+UPDATE animals SET species_id = (SELECT id FROM species WHERE name = 'digimon') WHERE name LIKE '%mon';
+
+select * from species_id;
+
+UPDATE animals SET species_id = (SELECT id FROM species WHERE name = 'pokemon') WHERE species_id IS NULL;
+
+select * from species_id;
+
+UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Sam Smith') WHERE name = 'Agumon';
+
+UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Jennifer Orwell') WHERE name IN ('Gabumon', 'Pikachu');
+
+UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Bob') WHERE name IN ('Devimon', 'Plantmon');
+
+UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Melody Pond') WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+
+UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Dean Winchester') WHERE name IN ('Angemon', 'Boarmon');
